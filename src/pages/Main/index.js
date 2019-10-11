e738a8e5cd422f3d8b1ef3ba78bdfff132d42357
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-11 20:40:18 -0300

    Inital Pages

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Main = () => <View />;
+
+Main.navigationOptions = {
+  title: 'Main',
+};
+
+export default Main;
