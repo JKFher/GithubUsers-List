1096debe6ac3e904f0c0e6887dad68d36560f06c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-14 22:07:49 -0300

    feat: Add components and style to home page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Keyboard} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {View} from 'react-native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  Name,
+  Bio,
+  ProfileButton,
+  ProfileButtonText,
+} from './styles';
 
-const Main = () => <View />;
+export default function Main() {
+  const [newUser, setNewUser] = useState('');
+  const [users, setUsers] = useState([]);
+
+  async function handleAddUser() {
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    setUsers([...users, data]);
+    setNewUser('');
+
+    Keyboard.dismiss();
+  }
+
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
+        />
+        <SubmitButton onPress={handleAddUser}>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({item}) => (
+          <User>
+            <Avatar source={{uri: item.avatar}} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+
+            <ProfileButton>
+              <ProfileButtonText>Open Profile</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
+    </Container>
+  );
+}
 
 Main.navigationOptions = {
-  title: 'Main',
+  title: 'Home',
 };
-
-export default Main;
