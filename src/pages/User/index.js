e4de81bf654256e657257107c850817a0717c6b8
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-16 23:00:42 -0300

    feat: Add loading and refreshing to Starred list

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {ActivityIndicator} from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -18,16 +19,39 @@ import {
 
 export default function User({navigation}) {
   const [starred, setStarred] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(2);
+  const [refreshing, setRefreshing] = useState(false);
   const user = navigation.getParam('user');
 
   useEffect(() => {
     async function getUserStarred() {
+      setLoading(true);
       const response = await api.get(`/users/${user.login}/starred`);
       setStarred(response.data);
+      setLoading(false);
     }
 
     getUserStarred();
   }, []);
+
+  async function loadMore() {
+    const response = await api.get(`/users/${user.login}/starred?page=${page}`);
+    const newPage = page + 1;
+    setPage(newPage);
+    setStarred([...starred, ...response.data]);
+  }
+
+  async function refreshList() {
+    setRefreshing(true);
+    const response = await api.get(`/users/${user.login}/starred`);
+    setStarred(response.data);
+    setRefreshing(false);
+  }
+
+  function handleNavigate(repository) {
+    navigation.navigate('Repository', {repository});
+  }
 
   return (
     <Container>
@@ -36,20 +60,27 @@ export default function User({navigation}) {
         <Name>{user.name}</Name>
         <Bio>{user.bio}</Bio>
       </Header>
-
-      <Stars
-        data={starred}
-        keyExtractor={star => String(star.id)}
-        renderItem={({item}) => (
-          <Starred>
-            <OwnerAvatar source={{uri: item.owner.avatar_url}} />
-            <Info>
-              <Title>{item.name}</Title>
-              <Author>{item.owner.login}</Author>
-            </Info>
-          </Starred>
-        )}
-      />
+      {loading ? (
+        <ActivityIndicator color="#161925" />
+      ) : (
+        <Stars
+          onRefresh={refreshList}
+          refreshing={refreshing}
+          onEndReachedThreshold={0.2}
+          onEndReached={loadMore}
+          data={starred}
+          keyExtractor={star => String(star.id)}
+          renderItem={({item}) => (
+            <Starred onPress={() => handleNavigate(item)}>
+              <OwnerAvatar source={{uri: item.owner.avatar_url}} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      )}
     </Container>
   );
 }
@@ -61,5 +92,6 @@ User.navigationOptions = ({navigation}) => ({
 User.propTypes = {
   navigation: PropTypes.shape({
     getParam: PropTypes.func,
+    navigate: PropTypes.func,
   }).isRequired,
 };
